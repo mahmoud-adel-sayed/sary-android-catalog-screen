2af2be4a09ca7b625864770dfbf5bec1b92e5a81
mahmoud-adel-sayed <mahmoudtaha939@gmail.com>
2021-09-12 13:13:45 +0200

    Added BottomBar Compose Previews.

--- app/src/main/java/com/sary/task/SaryApp.kt
+++ app/src/main/java/com/sary/task/SaryApp.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -26,7 +27,7 @@ fun SaryApp() {
                 val navController = rememberNavController()
                 Scaffold(
                     content = { NavGraph(navController) },
-                    bottomBar = { BottomBar(navController, mainTabs) }
+                    bottomBar = { BottomBar(navController = navController, tabs = mainTabs) }
                 )
             }
         }
@@ -34,12 +35,16 @@ fun SaryApp() {
 }
 
 @Composable
-private fun BottomBar(navController: NavController, tabs: Array<MainTabs>) {
+private fun BottomBar(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    tabs: Array<MainTabs>
+) {
     val currentBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = currentBackStackEntry?.destination?.route ?: MainTabs.STORE.route
 
     BottomNavigation(
-        modifier = Modifier.navigationBarsHeight(additional = 56.dp),
+        modifier = modifier.navigationBarsHeight(additional = 56.dp),
         backgroundColor = AppTheme.colors.surface
     ) {
         tabs.forEach { tab ->
@@ -66,3 +71,19 @@ private fun BottomBar(navController: NavController, tabs: Array<MainTabs>) {
 
 @Composable
 private fun rememberMainTabs(): Array<MainTabs> = remember { MainTabs.values() }
+
+@Preview(name = "Bottom Bar")
+@Composable
+private fun BottomBarPreview() {
+    AppTheme {
+        BottomBar(navController = rememberNavController(), tabs = rememberMainTabs())
+    }
+}
+
+@Preview(name = "Bottom Bar - Dark Theme")
+@Composable
+private fun BottomBarPreviewDark() {
+    AppTheme(darkTheme = true) {
+        BottomBar(navController = rememberNavController(), tabs = rememberMainTabs())
+    }
+}
